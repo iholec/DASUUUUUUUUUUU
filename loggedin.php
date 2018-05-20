b498e80fcce52874b5174e72578f28b1d245157b
Selina Brinnich <if16b030@technikum-wien.at>
2018-05-20 15:30:27 +0200

    Added session start in additional files

--- loggedin.php
+++ loggedin.php
@@ -1,5 +1,5 @@
 
 <form id="loginout" method="POST" action="logout.php">
-<input type="hidden" name="csrf" value="<?php echo $_SESSION['csrf_token'];?>"/>
+	<input type="hidden" name="csrf" value="<?php echo $_SESSION['csrf_token'];?>"/>
 	<input type="submit" id="logoutbutton" name="submit" value="Logout!"/>
 </form>
